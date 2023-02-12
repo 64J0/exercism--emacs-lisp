2711a21ccb86b2194a016b3116e6b3cd7f9dcb92
64J0 <vinigaio97@gmail.com>
2023-02-11 21:45:24 -0300

    fix: 01-hello-world.el code

--- submissions/01-hello-world.el
+++ submissions/01-hello-world.el
@@ -1,12 +1,19 @@
--*- lexical-binding: t; -*-
+;;; 01-hello-world.el --- Hello World Exercise (exercism) -*- lexical-binding: t; -*-
+
+;;; Commentary:
 
 ;; “Hello, World!” will get you writing some Emacs Lisp and familiarise yourself
-;; with the Exercism workflow. Completing it unlocks the rest of the Emacs Lisp
+;; with the Exercism workflow.  Completing it unlocks the rest of the Emacs Lisp
 ;; Track.
 
+;;; Code:
+
 (defun hello ()
-  "Simple hello world function. The output is simply the string
-  `Hello, World!'"
+  "Simple 'Hello World' function.  The output is simply the
+string `Hello, World!'"
   "Hello, World!")
 
 (hello)
+
+(provide 'hello)
+;;; 01-hello-world.el ends here
