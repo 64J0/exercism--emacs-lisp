bf7a0039fa2913b62c49272f2957054e84b7079f
64J0 <vinigaio97@gmail.com>
2023-02-14 20:05:11 -0300

    chore: add more comments related to the 6th submission

--- submissions/06-rna-transcription.el
+++ submissions/06-rna-transcription.el
@@ -6,9 +6,27 @@
 ;; https://github.com/bbatsov/emacs-lisp-style-guide#naming
 ;; https://github.com/bbatsov/emacs-lisp-style-guide#docstrings
 
+;; For functions that are defined in the C code no packages needs to be
+;; required.
+
+;; This is not really an improvement, just a variation: You can use `mapconcat'
+;; to map a sequence and create a String as result.  `mapconcat' expects a
+;; separator though, so you wouldn't even save a line of code.
+
+;; Since `mapconcat' is defined in the C code it might be faster than the
+;; combination of `string-join' and `seq-map', might also only iterate the
+;; sequence once.  I wouldn't think about this stuff though unless you're
+;; running into performance problems and benchmarks point into that direction.
+
+;; About the comments in the program structure:
+
+;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Simple-Packages.html
+;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Library-Headers.html
+
 ;;; Code:
 
 (require 'subr-x)
+(require 'seq)
 
 (defconst nucleotide-complement
   '((?G . "C")
@@ -27,7 +45,7 @@
     dna-strand)))
 
 ;; Test
-;; (to-rna "GCTAX")
+;; (to-rna "GCTA")
 
 (provide 'rna-transcription)
 ;;; 06-rna-transcription.el ends here
